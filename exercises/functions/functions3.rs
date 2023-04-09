aa2356383d191c64e90f47479b3620d13b9cbef6
zhaozihanzzh <zhaozihanrxh@163.com>
2023-04-09 17:10:00 +0800

    Finish functions, if and quiz1

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -1,10 +1,10 @@
 // functions3.rs
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    call_me();
+    let neg = -200;
+    call_me(neg as u8 as u32); // 直接传入 -1 不行，不能自动进行隐式转换；同时不能写 -200 as u8，原因未知
 }
 
 fn call_me(num: u32) {
