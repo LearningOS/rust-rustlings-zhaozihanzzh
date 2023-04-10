38c81010580f63b98b94e8141ff1f12e61f0e07a
zhaozihanzzh <zhaozihanrxh@163.com>
2023-04-10 23:58:50 +0800

    Finish primitive types

--- exercises/primitive_types/primitive_types4.rs
+++ exercises/primitive_types/primitive_types4.rs
@@ -2,13 +2,12 @@
 // Get a slice out of Array a where the ??? is so that the test passes.
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..=3]; // 这里要有借用，因为我觉得我们不能直接转移所有权。
 
     assert_eq!([2, 3, 4], nice_slice)
 }
