b5a11b1487ba3234846c07ae384966b69763fcce
zhaozihanzzh <zhaozihanrxh@163.com>
2023-04-16 18:08:47 +0800

    Finish arc, rc, cow, threads, macros, clippy and conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,26 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        match s.find(',') {
+            Some(location) => {
+                let name = &s[0..location];
+                if name.len() == 0 {
+                    Person::default()
+                } else {
+                    match &s[(location+1)..].parse::<usize>() {
+                        Ok(age) => Person{name : name.to_string(), age: *age},
+                        Err(_) => Person::default(),
+                    }
+                }
+            },
+            None => Person::default()
+        }
     }
 }
 
